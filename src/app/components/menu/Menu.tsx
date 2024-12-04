365cfe7fdfd9a064b0be97a8a8913003402e6a03
OLDmax007 <maxsim.dobrovolskyimd@gmail.com>
2024-12-05 00:17:33 +0200

    Updated and add new styles to  css files like global and user-info

--- src/app/components/menu/Menu.tsx
+++ src/app/components/menu/Menu.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import MenuLink from "@/app/components/menu-link/MenuLink";
+import styles from  './menu.module.css'
 
 const Menu = () => {
     return (
         <nav>
-            <ul>
+            <ul className={styles.menu}>
             <li><MenuLink path={'/'}>Home</MenuLink></li>
             <li><MenuLink path={'/movies'}>Movies</MenuLink></li>
             <li><MenuLink path={'/#about'}>About</MenuLink></li>
