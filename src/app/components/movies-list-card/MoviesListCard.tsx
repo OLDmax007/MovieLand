7a0f83b1111ae818dcc5fc52e20e8689a965dd63
OLDmax007 <maxsim.dobrovolskyimd@gmail.com>
2024-12-05 00:20:36 +0200

    Created 'MoviesListCard' component

---
+++ src/app/components/movies-list-card/MoviesListCard.tsx
@@ -0,0 +1,17 @@
+import React, {FC} from 'react';
+import {IMovie} from "@/app/models/IMovie";
+
+interface IMoviesListCardProps {
+    movie: IMovie
+}
+
+
+const MoviesListCard:FC<IMoviesListCardProps> = () => {
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default MoviesListCard;
