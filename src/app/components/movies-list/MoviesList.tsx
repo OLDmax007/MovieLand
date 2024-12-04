d49f90f8225f465f1a3c4d794feffe109ff635a5
OLDmax007 <maxsim.dobrovolskyimd@gmail.com>
2024-12-05 00:25:09 +0200

    Updated 'MoviesListCard' component

--- src/app/components/movies-list/MoviesList.tsx
+++ src/app/components/movies-list/MoviesList.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import moviesService from "@/app/services/tmdb.api.service";
 import {IMovie} from "@/app/models/IMovie";
+import MoviesListCard from "@/app/components/movies-list-card/MoviesListCard";
 
 const MoviesList = async () => {
     const movies:IMovie[] = await moviesService.getAllMovies()
 
     return (
         <div>
-
+            {movies.map((movie:IMovie, index:number) => (
+                <MoviesListCard key={index} movie={movie}/>
+            ))}
         </div>
     );
 };
