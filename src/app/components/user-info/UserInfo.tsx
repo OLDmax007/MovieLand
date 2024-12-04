806f0fbb0c03522901ebf7aec40b85bb12762a49
OLDmax007 <maxsim.dobrovolskyimd@gmail.com>
2024-12-04 18:40:26 +0200

    Updated 'UserInfo' component

--- src/app/components/user-info/UserInfo.tsx
+++ src/app/components/user-info/UserInfo.tsx
@@ -2,8 +2,9 @@ import React from 'react';
 
 const UserInfo = () => {
     return (
-        <div>
-            
+        <div className={'user-info'}>
+            <span>Maxsim Dobrovolskyi</span>
+            <div><img src={'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRYDoS6cUbWHJILYpRGvE9o3JxLUtHmk2Euqw&s'}/></div>
         </div>
     );
 };
