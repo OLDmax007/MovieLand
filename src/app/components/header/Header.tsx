3981370d4585e7d4f17d00149f08b65aead3df99
OLDmax007 <maxsim.dobrovolskyimd@gmail.com>
2024-12-04 19:03:05 +0200

    Created css file for 'UserInfo' and stylization 'UserInfo' component

--- src/app/components/header/Header.tsx
+++ src/app/components/header/Header.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Menu from "@/app/components/menu/Menu";
+import UserInfo from "@/app/components/user-info/UserInfo";
 
 const Header = () => {
     return (
         <header>
             <h1>MovieLand</h1>
             <Menu/>
+            <UserInfo/>
         </header>
     );
 };
--- src/app/components/user-info/UserInfo.tsx
+++ src/app/components/user-info/UserInfo.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
+import styles from './user-info.module.css'
 
 const UserInfo = () => {
     return (
-        <div className={'user-info'}>
+        <div className={styles.userInfo}>
+            <div className={styles.userDesc}>
             <span>Maxsim Dobrovolskyi</span>
-            <div><img src={'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRYDoS6cUbWHJILYpRGvE9o3JxLUtHmk2Euqw&s'}/></div>
+            <p>Developer</p>
+            </div>
+            <div className={styles.userAvatar}><img src={'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRYDoS6cUbWHJILYpRGvE9o3JxLUtHmk2Euqw&s'} alt={'User avatar'}/></div>
         </div>
     );
 };
