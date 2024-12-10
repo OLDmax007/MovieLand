2dd59bdaa6ace2543a1aa5d704ea17c9a4b4d39f
OLDmax007 <maxsim.dobrovolskyimd@gmail.com>
2024-12-10 23:34:51 +0200

    Changed  file 'urls'

--- src/app/constans/urls.ts
+++ src/app/constans/urls.ts
@@ -1,17 +1,25 @@
+const tmdbUrl: string = 'https://api.themoviedb.org/3/'
+
+const params = {
+    pmPage: '?page=',
+    pmWithGenres: '?with_genres=',
+}
+
 const urls = {
+    tmdbUrl: tmdbUrl,
+
     movies: {
-        moviesBaseUrl: 'https://api.themoviedb.org/3/discover/movie',
-        paramPage: '?page='
+        moviesUrl: tmdbUrl + 'discover/movie',
     },
 
     genres: {
-        genresBaseUrl: 'https://api.themoviedb.org/3/genre/movie/list'
+        genresUrl: tmdbUrl + 'genre/movie/list',
+        byGenre: (genreId: number): string => tmdbUrl + urls.movies.moviesUrl + params.pmWithGenres + genreId,
     },
 
     poster: {
-        posterBaseUrl: 'https://image.tmdb.org/t/p/original/'
+        posterUrl: 'https://image.tmdb.org/t/p/original/'
     }
-
 }
 
-export default urls
+export default urls;
