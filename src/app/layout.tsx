6620a923524d529d269e02e23fd5d4f8c58f09bd
OLDmax007 <maxsim.dobrovolskyimd@gmail.com>
2025-01-05 10:20:38 +0200

    some updated (not meter)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,6 @@ export default function RootLayout({children,}: Readonly<{
         <html lang="en">
         <Head>
             <link rel="icon" href="/favicon.ico"/>
-            <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet"/>
         </Head>
         <body>
         <Header/>
